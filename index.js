44966efba52f7459b13d968859beaa974cca2ae7
Michelle Too <mjxtcodes@gmail.com>
2022-10-11 15:49:14 -0700

    commit tree b66d83bbed249e482632d249032b5231b8de9287

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+var customerName = 'bob'
+return customerName
+function upperCaseCustomerName() = to.UpperCase(customerName)
+function setBestCustomer() {
+}
+let bestCustomer = 'not bob'
+function overwriteBestCustomer() {
+    let bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'something'
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'something else'
+}
